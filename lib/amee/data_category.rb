41d5f88ae8418e8426f67eb71a9929994a26f681
James Smith <james@floppy.org.uk>
2008-07-08 19:20:31 +0100

    added initialize and path for DataCategory

--- lib/amee/data_category.rb
+++ lib/amee/data_category.rb
@@ -1,6 +1,14 @@
 module AMEE
   module Data
     class Category < AMEE::Object
+
+      def initialize(data = {})
+        @path = data ? data[:path] : nil
+        super
+      end
+
+      attr_reader :path
+      
     end
   end
 end
--- spec/data_category_spec.rb
+++ spec/data_category_spec.rb
@@ -2,9 +2,28 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe AMEE::Data::Category do
   
+  before(:each) do
+    @cat = AMEE::Data::Category.new    
+  end
+  
   it "should have common AMEE object properties" do
-    x = AMEE::Data::Category.new
-    x.is_a?(AMEE::Object).should be_true
+    @cat.is_a?(AMEE::Object).should be_true
+  end
+  
+  it "should have a path" do
+    @cat.should respond_to(:path)
+  end
+
+  it "should initialize AMEE::Object data on creation" do
+    uid = 'ABCD1234'
+    @cat = AMEE::Data::Category.new(:uid => uid)
+    @cat.uid.should == uid
+  end
+
+  it "can be created with data" do
+    path = "/data"
+    @cat = AMEE::Data::Category.new(:path => path)
+    @cat.path.should == path
   end
   
 end
