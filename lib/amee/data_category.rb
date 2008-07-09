ed102032e21414a05165aa2016189abd89bef03d
James Smith <james@floppy.org.uk>
2008-07-09 06:45:56 +0100

    added correct data to DataCategory

--- lib/amee/data_category.rb
+++ lib/amee/data_category.rb
@@ -4,10 +4,22 @@ module AMEE
 
       def initialize(data = {})
         @path = data ? data[:path] : nil
+        @name = data ? data[:name] : nil
+        @children = data ? data[:children] : []
+        @items = data ? data[:items] : []
         super
       end
 
       attr_reader :path
+      attr_reader :name
+      attr_reader :children
+      attr_reader :items
+
+    protected
+      
+      def full_path
+        "/data#{@path}"
+      end
       
     end
   end
--- spec/data_category_spec.rb
+++ spec/data_category_spec.rb
@@ -14,16 +14,38 @@ describe AMEE::Data::Category do
     @cat.should respond_to(:path)
   end
 
+  it "should have a full path" do
+    @cat.should respond_to(:full_path)
+  end
+
+  it "should have a name" do
+    @cat.should respond_to(:name)
+  end
+
+  it "should have children" do
+    @cat.should respond_to(:children)
+  end
+
+  it "should have items" do
+    @cat.should respond_to(:items)
+  end
+
   it "should initialize AMEE::Object data on creation" do
     uid = 'ABCD1234'
     @cat = AMEE::Data::Category.new(:uid => uid)
     @cat.uid.should == uid
   end
 
-  it "can be created with data" do
-    path = "/data"
-    @cat = AMEE::Data::Category.new(:path => path)
+  it "can be created with hash of data" do
+    path = ""
+    name = "Root"
+    children = ["one", "two"]
+    items = ["three", "four"]
+    @cat = AMEE::Data::Category.new(:path => path, :name => name, :children => children, :items => items)
     @cat.path.should == path
+    @cat.name.should == name
+    @cat.children.should == children
+    @cat.items.should == items
   end
   
 end
