d9895af6691482c0847b342ecbac8e4a04a7497e
James Smith <james@floppy.org.uk>
2008-07-10 23:36:12 +0100

    implement data item parsing

---
+++ examples/view_data_item.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'amee'
+require 'optparse'
+
+# Command-line options - get username, password, and server
+options = {}
+OptionParser.new do |opts|
+  opts.on("-u", "--username USERNAME", "AMEE username") do |u|
+    options[:username] = u
+  end  
+  opts.on("-p", "--password PASSWORD", "AMEE password") do |p|
+    options[:password] = p
+  end
+  opts.on("-s", "--server SERVER", "AMEE server") do |s|
+    options[:server] = s
+  end
+end.parse!
+
+# Connect
+connection = AMEE::Connection.new(options[:server], options[:username], options[:password])
+
+# For each path in arg list, show details
+ARGV.each do |path|
+  cat = AMEE::Data::Item.get(connection, path)
+  puts "---------------------"
+  puts "Name: #{cat.name}"
+  puts "Path: #{cat.full_path}"
+  puts "Label: #{cat.label}"
+  puts "UID: #{cat.uid}"
+  puts "Values:"
+  cat.values.each do |v|
+    puts "  - #{v[:name]}: #{v[:value]}"
+  end
+  
+end
+
+
--- lib/amee/data_item.rb
+++ lib/amee/data_item.rb
@@ -4,10 +4,42 @@ module AMEE
 
       def initialize(data = {})
         @values = data ? data[:values] : []
+        @label = data ? data[:label] : []
         super
       end
 
       attr_reader :values
+      attr_reader :label
+
+      def self.get(connection, path)
+        # Load data from path
+        response = connection.get(path)
+        # Parse data from response into hash
+        doc = REXML::Document.new(response)
+        data = {}
+        data[:uid] = REXML::XPath.first(doc, "/Resources/DataItemResource/DataItem/@uid").to_s
+        data[:created] = DateTime.parse(REXML::XPath.first(doc, "/Resources/DataItemResource/DataItem/@created").to_s)
+        data[:modified] = DateTime.parse(REXML::XPath.first(doc, "/Resources/DataItemResource/DataItem/@modified").to_s)
+        data[:name] = REXML::XPath.first(doc, '/Resources/DataItemResource/DataItem/Name').text
+        data[:path] = REXML::XPath.first(doc, '/Resources/DataItemResource/Path').text
+        data[:label] = REXML::XPath.first(doc, '/Resources/DataItemResource/DataItem/Label').text
+        # Get values
+        data[:values] = []
+        REXML::XPath.each(doc, '/Resources/DataItemResource/DataItem/ItemValues/ItemValue') do |value|
+          value_data = {}
+          value_data[:name] = value.elements['Name'].text
+          value_data[:path] = value.elements['Path'].text
+          value_data[:value] = value.elements['Value'].text
+          value_data[:uid] = value.attributes['uid'].to_s
+          data[:values] << value_data
+        end
+        # Create item object
+        item = Item.new(data)
+        # Store connection in object for future use
+        item.connection = connection
+        # Done
+        return item
+      end
 
     end
   end
