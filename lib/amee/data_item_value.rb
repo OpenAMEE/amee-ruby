c5f19076b168d9fc87965e0920289da116b74c4b
James Smith <james@floppy.org.uk>
2008-07-11 08:22:48 +0100

    implement DataItemValue parsing

--- lib/amee/data_item_value.rb
+++ lib/amee/data_item_value.rb
@@ -9,6 +9,26 @@ module AMEE
 
       attr_reader :value
       
+      def self.get(connection, path)
+        # Load data from path
+        response = connection.get(path)
+        # Parse data from response into hash
+        doc = REXML::Document.new(response)
+        data = {}
+        data[:uid] = REXML::XPath.first(doc, "/Resources/DataItemValueResource/ItemValue/@uid").to_s
+        data[:created] = DateTime.parse(REXML::XPath.first(doc, "/Resources/DataItemValueResource/ItemValue/@Created").to_s)
+        data[:modified] = DateTime.parse(REXML::XPath.first(doc, "/Resources/DataItemValueResource/ItemValue/@Modified").to_s)
+        data[:name] = REXML::XPath.first(doc, '/Resources/DataItemValueResource/ItemValue/Name').text
+        data[:path] = path
+        data[:value] = REXML::XPath.first(doc, '/Resources/DataItemValueResource/ItemValue/Value').text
+        # Create item object
+        item = ItemValue.new(data)
+        # Store connection in object for future use
+        item.connection = connection
+        # Done
+        return item
+      end
+
     end
   end
 end
--- spec/data_item_value_spec.rb
+++ spec/data_item_value_spec.rb
@@ -27,3 +27,19 @@ describe AMEE::Data::ItemValue do
   end
 
 end
+
+describe AMEE::Data::ItemValue, "with an authenticated connection" do
+
+  it "should parse correctly" do
+    connection = flexmock "connection"
+    connection.should_receive(:get).with("/data/transport/plane/generic/AD63A83B4D41/kgCO2PerPassengerJourney").and_return('<Resources><DataItemValueResource><ItemValue Created="2007-08-01 09:00:41.0" Modified="2007-08-01 09:00:41.0" uid="127612FA4921"><Path>kgCO2PerPassengerJourney</Path><Name>kgCO2 Per Passenger Journey</Name><Value>0</Value><ItemValueDefinition uid="653828811D42"><Path>kgCO2PerPassengerJourney</Path><Name>kgCO2 Per Passenger Journey</Name><FromProfile>false</FromProfile><FromData>true</FromData><ValueDefinition uid="8CB8A1789CD6"><Name>kgCO2PerJourney</Name><ValueType>DECIMAL</ValueType></ValueDefinition></ItemValueDefinition><DataItem uid="AD63A83B4D41"/></ItemValue><DataItem uid="AD63A83B4D41"/></DataItemValueResource></Resources>')
+    @value = AMEE::Data::ItemValue.get(connection, "/data/transport/plane/generic/AD63A83B4D41/kgCO2PerPassengerJourney")
+    @value.uid.should == "127612FA4921"
+    @value.name.should == "kgCO2 Per Passenger Journey"
+    @value.path.should == "/data/transport/plane/generic/AD63A83B4D41/kgCO2PerPassengerJourney"
+    @value.created.should == DateTime.new(2007,8,1,9,00,41)
+    @value.modified.should == DateTime.new(2007,8,1,9,00,41)
+    @value.value.should == "0"
+  end
+
+end
