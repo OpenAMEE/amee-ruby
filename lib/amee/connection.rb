5479f08fe7c811ecd4aa47ed23fa8aaf825126aa
James Smith <james@floppy.org.uk>
2008-07-21 19:12:22 +0100

    Mock Net::HTTP interactions in connection spec. Changed connection code slightly to allow mocking, but no change to functionality.

--- lib/amee/connection.rb
+++ lib/amee/connection.rb
@@ -27,20 +27,20 @@ module AMEE
       response = nil
       http = Net::HTTP.new(@server)
       #http.set_debug_output($stdout)
-      http.start do
-        get = Net::HTTP::Get.new(path)
+      http.start
+      get = Net::HTTP::Get.new(path)
+      get['authToken'] = @auth_token
+      get['Accept'] = 'application/xml'
+      response = http.request(get)
+      # Handle 404s
+      raise AMEE::NotFound.new("URL doesn't exist on server.") if response.code == '404'
+      # If request fails, authenticate and try again
+      if authentication_failed?(response)
+        authenticate
         get['authToken'] = @auth_token
-        get['Accept'] = 'application/xml'
         response = http.request(get)
-        # Handle 404s
-        raise AMEE::NotFound.new("URL doesn't exist on server.") if response.code == '404'
-        # If request fails, authenticate and try again
-        if authentication_failed?(response)
-          authenticate
-          get['authToken'] = @auth_token
-          response = http.request(get)
-        end
       end
+      http.finish
       yield response.body if block_given?
       response.body
     rescue SocketError
@@ -59,14 +59,14 @@ module AMEE
       end
       response = nil
       http = Net::HTTP.new(@server)
-      http.start do
-        post = Net::HTTP::Post.new("/auth")
-        post.body = "username=#{@username}&password=#{@password}"
-        post['Accept'] = 'application/xml'
-        response = http.request(post)
-        @auth_token = response['authToken']
-        raise AMEE::AuthFailed.new("Authentication failed. Please check your username and password.") unless authenticated?
-      end
+      http.start
+      post = Net::HTTP::Post.new("/auth")
+      post.body = "username=#{@username}&password=#{@password}"
+      post['Accept'] = 'application/xml'
+      response = http.request(post)
+      @auth_token = response['authToken']
+      raise AMEE::AuthFailed.new("Authentication failed. Please check your username and password.") unless authenticated?
+      http.finish
     rescue SocketError
       raise AMEE::ConnectionFailed.new("Connection failed. Check server name or network connection.")
     end
--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -44,6 +44,14 @@ describe AMEE::Connection, "with authentication" do
   end
 
   it "should be able to get private URLs" do
+    flexmock(Net::HTTP).new_instances do |mock|
+      mock.should_receive(:start => nil)
+      mock.should_receive(:request).and_return(#flexmock(:code => '401', :body => ''),
+                                               #flexmock(:code => '200', :body => '', :'[]' => 'dummy_auth_token_data'),
+                                               #flexmock(:code => '200', :body => '<?xml version="1.0" encoding="UTF-8"?><Resources><DataCategoryResource><Path/><DataCategory created="2007-07-27 09:30:44.0" modified="2007-07-27 09:30:44.0" uid="CD310BEBAC52"><Name>Root</Name><Path/><Environment uid="5F5887BCF726"/></DataCategory><Children><DataCategories><DataCategory uid="BBA3AC3E795E"><Name>Home</Name><Path>home</Path></DataCategory><DataCategory uid="9E5362EAB0E7"><Name>Metadata</Name><Path>metadata</Path></DataCategory><DataCategory uid="6153F468BE05"><Name>Test</Name><Path>test</Path></DataCategory><DataCategory uid="263FC0186834"><Name>Transport</Name><Path>transport</Path></DataCategory><DataCategory uid="2957AE9B6E6B"><Name>User</Name><Path>user</Path></DataCategory></DataCategories></Children></DataCategoryResource></Resources>'))
+                                               flexmock(:code => '401', :body => '<?xml version="1.0" encoding="UTF-8"?><Resources><DataCategoryResource><Path/><DataCategory created="2007-07-27 09:30:44.0" modified="2007-07-27 09:30:44.0" uid="CD310BEBAC52"><Name>Root</Name><Path/><Environment uid="5F5887BCF726"/></DataCategory><Children><DataCategories><DataCategory uid="BBA3AC3E795E"><Name>Home</Name><Path>home</Path></DataCategory><DataCategory uid="9E5362EAB0E7"><Name>Metadata</Name><Path>metadata</Path></DataCategory><DataCategory uid="6153F468BE05"><Name>Test</Name><Path>test</Path></DataCategory><DataCategory uid="263FC0186834"><Name>Transport</Name><Path>transport</Path></DataCategory><DataCategory uid="2957AE9B6E6B"><Name>User</Name><Path>user</Path></DataCategory></DataCategories></Children></DataCategoryResource></Resources>', :'[]' => 'dummy_auth_token_data'))
+      mock.should_receive(:finish => nil)
+    end
     @amee.get('/data') do |response|
       response.should_not be_empty
     end
@@ -51,6 +59,7 @@ describe AMEE::Connection, "with authentication" do
   end
 
   it "should handle 404s gracefully" do
+    flexmock(Net::HTTP).new_instances.should_receive(:start => nil, :request => flexmock(:code => '404', :body => ""), :finish => nil)
     lambda{@amee.get('/missing_url')}.should raise_error(AMEE::NotFound, "URL doesn't exist on server.")
   end
 
@@ -74,10 +83,12 @@ describe AMEE::Connection, "with bad authentication information" do
   end
   
   it "should be capable of making requests for public URLs" do
+    flexmock(Net::HTTP).new_instances.should_receive(:start => nil, :request => flexmock(:code => '200', :body => ""), :finish => nil)
     lambda{@amee.get('/')}.should_not raise_error
   end
 
   it "should get an authentication failure when accessing private URLs" do
+    flexmock(Net::HTTP).new_instances.should_receive(:start => nil, :request => flexmock(:code => '401', :body => "", :'[]' => nil), :finish => nil)
     lambda{@amee.get('/data')}.should raise_error(AMEE::AuthFailed, "Authentication failed. Please check your username and password.")
   end
 
@@ -96,6 +107,7 @@ describe AMEE::Connection, "without authentication" do
   end
 
   it "should be capable of making requests for public URLs" do
+    flexmock(Net::HTTP).new_instances.should_receive(:start => nil, :request => flexmock(:code => '200', :body => ""), :finish => nil)
     @amee.get('/') do |response|
       response.should be_empty
     end
@@ -103,6 +115,7 @@ describe AMEE::Connection, "without authentication" do
   end
 
   it "should not be able to get private URLs" do
+    flexmock(Net::HTTP).new_instances.should_receive(:start => nil, :request => flexmock(:code => '401', :body => ""), :finish => nil)
     lambda{@amee.get('/data')}.should raise_error(AMEE::AuthRequired, "Authentication required. Please provide your username and password.")
   end
 
