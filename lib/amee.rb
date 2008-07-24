3f6f17dadaffe0bc58ec508dc1c0f900bf82760d
James Smith <james@floppy.org.uk>
2008-07-24 10:52:57 +0100

    include profile from main amee file

--- lib/amee.rb
+++ lib/amee.rb
@@ -5,13 +5,14 @@ require 'amee/object'
 require 'amee/data_category'
 require 'amee/data_item'
 require 'amee/data_item_value'
+require 'amee/profile'
 
 module AMEE
   
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 1
-    TINY  = 1
+    TINY  = 2
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
   
