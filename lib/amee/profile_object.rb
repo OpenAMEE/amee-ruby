69402ca6610f3bfbdac60b72581638df0f9cc7cb
James Smith <james@floppy.org.uk>
2008-09-03 18:24:04 +0100

    Add generic profile-related information to Profile::Object

--- lib/amee/profile_object.rb
+++ lib/amee/profile_object.rb
@@ -1,9 +1,18 @@
 module AMEE
   module Profile
     class Object < AMEE::Object
-    
+
+      def initialize(options = {})
+        @profile_uid = options[:profile_uid]
+        @profile_date = options[:profile_date]
+        super
+      end
+
+      attr_reader :profile_uid
+      attr_reader :profile_date
+
       def full_path
-        "/profiles#{@path}"
+        "/profiles#{'/' if @profile_uid}#{@profile_uid}#{@path}"
       end
       
     end
--- spec/profile_object_spec.rb
+++ spec/profile_object_spec.rb
@@ -6,4 +6,19 @@ describe AMEE::Profile::Object do
     AMEE::Profile::Object.new.full_path.should == "/profiles"
   end
 
+  it "can have a profile UID" do
+    obj = AMEE::Profile::Object.new(:profile_uid => 'ABC123')
+    obj.profile_uid.should == "ABC123"
+  end
+
+  it "should create correct path if profile UID is set" do
+    obj = AMEE::Profile::Object.new(:profile_uid => 'ABC123')
+    obj.full_path.should == "/profiles/ABC123"
+  end
+
+  it "can have a profile date" do
+    obj = AMEE::Profile::Object.new(:profile_date => DateTime.new(2008,01))
+    obj.profile_date.should == DateTime.new(2008,01)
+  end
+
 end
