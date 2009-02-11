6a874227a13737f3684a77326dcdc77593b6d2b1
James Smith <james@floppy.org.uk>
2009-02-11 12:12:28 +0000

    Add some v2 utility functions

--- lib/amee.rb
+++ lib/amee.rb
@@ -11,6 +11,12 @@ class String
   def is_json?
     slice(0,1) == '{'
   end
+  def is_xml?
+    slice(0,5) == '<?xml'
+  end
+  def is_v2_xml?
+    is_xml? && include?('<Resources xmlns="http://schemas.amee.cc/2.0">')
+  end
 end
 
 require 'amee/version'
@@ -26,3 +32,27 @@ require 'amee/profile'
 require 'amee/profile_category'
 require 'amee/profile_item'
 require 'amee/drill_down'
+
+class Date
+  def xmlschema
+    strftime("%Y-%m-%dT%H:%M+0000")
+  end
+  def amee1_date
+    strftime("%Y%m%d")
+  end
+  def amee1_month
+    strftime("%Y%m")
+  end
+end
+
+class Time
+  def xmlschema
+    strftime("%Y-%m-%dT%H:%M+0000")
+  end
+  def amee1_date
+    strftime("%Y%m%d")
+  end
+  def amee1_month
+    strftime("%Y%m")
+  end
+end
