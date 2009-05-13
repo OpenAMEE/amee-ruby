99d6d46313f21098fff43794904031e372336441
Diggory Briercliffe <dig@MBP-DIG.local>
2009-05-13 19:12:28 +0100

    Item Definitions.

--- lib/amee/item_definition.rb
+++ lib/amee/item_definition.rb
@@ -57,6 +57,19 @@ module AMEE
         ItemDefinitionList.new(connection)
       end
 
+      def self.parse(connection, response, is_list = true)
+        # Parse data from response
+        if response.is_json?
+          item_definition = ItemDefinition.from_json(response)
+        else
+          item_definition = ItemDefinition.from_xml(response, is_list)
+        end
+        # Store connection in object for future use
+        item_definition.connection = connection
+        # Done
+        return item_definition
+      end
+
       def self.from_json(json)
         # Read JSON
         doc = JSON.parse(json)
@@ -71,14 +84,15 @@ module AMEE
         raise AMEE::BadData.new("Couldn't load ItemDefinition from JSON. Check that your URL is correct.")
       end
 
-      def self.from_xml(xml)
+      def self.from_xml(xml, is_list = true)
+        prefix = is_list ? "/Resources/ItemDefinitionsResource/" : "/Resources/ItemDefinitionResource/"
         # Parse data from response into hash
         doc = REXML::Document.new(xml)
         data = {}
-        data[:uid] = REXML::XPath.first(doc, "/Resources/ItemDefinitionResource/ItemDefinition/@uid").to_s
-        data[:created] = DateTime.parse(REXML::XPath.first(doc, "/Resources/ItemDefinitionResource/ItemDefinition/@created").to_s)
-        data[:modified] = DateTime.parse(REXML::XPath.first(doc, "/Resources/ItemDefinitionResource/ItemDefinition/@modified").to_s)
-        data[:name] = REXML::XPath.first(doc, '/Resources/ItemDefinitionResource/ItemDefinition/Name').text
+        data[:uid] = REXML::XPath.first(doc, prefix + "ItemDefinition/@uid").to_s
+        data[:created] = DateTime.parse(REXML::XPath.first(doc, prefix + "ItemDefinition/@created").to_s)
+        data[:modified] = DateTime.parse(REXML::XPath.first(doc, prefix + "ItemDefinition/@modified").to_s)
+        data[:name] = REXML::XPath.first(doc, prefix + "ItemDefinition/Name").text
         # Create object
         ItemDefinition.new(data)
       rescue
@@ -88,14 +102,8 @@ module AMEE
       def self.get(connection, path, options = {})
         # Load data from path
         response = connection.get(path, options).body
-        # Parse data from response
-        if response.is_json?
-          item_definition = ItemDefinition.from_json(response)
-        else
-          item_definition = ItemDefinition.from_xml(response)
-        end
-        # Store connection in object for future use
-        item_definition.connection = connection
+        # Parse response
+        item_definition = ItemDefinition.parse(connection, response, false)
         # Done
         return item_definition
       rescue
@@ -106,6 +114,20 @@ module AMEE
         response = connection.put(full_path, options).body
       rescue
         raise AMEE::BadData.new("Couldn't update ItemDefinition. Check that your information is correct.")
+      end
+
+      def self.create(connection, options = {})
+        unless options.is_a?(Hash)
+          raise AMEE::ArgumentError.new("Second argument must be a hash of options!")
+        end
+        # Send data
+        response = connection.post("/admin/itemDefinitions", options).body
+        # Parse response
+        item_definition = ItemDefinition.parse(connection, response)
+        # Get the ItemDefinition again
+        return ItemDefinition.get(connection, "/admin/itemDefinitions/" + item_definition.uid)
+      rescue
+        raise AMEE::BadData.new("Couldn't create ItemDefinition. Check that your information is correct.")
       end
 
     end
