f493108729c1dcd7ee740d191fa053549a29af8f
James Smith <james@floppy.org.uk>
2009-10-01 14:15:13 +0100

    Don't extend ActiveRecord if it doesn't exist!

--- rails/init.rb
+++ rails/init.rb
@@ -6,8 +6,6 @@ amee_config = "#{RAILS_ROOT}/config/amee.yml"
 if File.exist?(amee_config)
   # Load config
   $AMEE_CONFIG = YAML.load_file(amee_config)[RAILS_ENV]
-  # Create a global AMEE connection that we can use from anywhere in this app
-  AMEE::Rails.establish_connection($AMEE_CONFIG['server'], $AMEE_CONFIG['username'], $AMEE_CONFIG['password'])
 end
 
 # Add AMEE extensions into ActiveRecord::Base
