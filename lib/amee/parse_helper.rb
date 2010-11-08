45a9d82278a502daff9ef4309676b7a0a3b69718
James Smith <james@floppy.org.uk>
2010-11-08 16:14:58 +0000

    Handle xpath selectors which return strings

--- lib/amee/parse_helper.rb
+++ lib/amee/parse_helper.rb
@@ -7,21 +7,26 @@ module ParseHelper
     else
       nodes=REXML::XPath.match(doc,"#{preamble}#{xpath}")
     end
+    if nodes.is_a? String
+      return nodes
+    end
     if nodes.length==1
       return node_value(nodes.first)
     elsif nodes.length>1
       return nodes.map{|x| node_value(x)}
+    else
+      return nil
     end
-    return nil
-
   end
   
   def node_value(node)
     if node.is_a?(Nokogiri::XML::Attr) || node.is_a?(REXML::Attribute)
       # Attributes are allowed to be an empty string
       return node.to_s
-    else
+    elsif node.is_a?(Nokogiri::XML::Element) || node.is_a?(REXML::Element)
       return node.text == '' ? nil : node.text
+    else
+      return node.to_s
     end
   end
 
