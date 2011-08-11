2c69aacda447b54801ac60b250015e93d2daa90d
James Smith <james@floppy.org.uk>
2011-08-11 16:00:09 +0100

    Add standard file headers SDK-164

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
+# Copyright (C) 2008-2011 AMEE UK Ltd. - http://www.amee.com
+# Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
+
 require File.dirname(__FILE__) + "/rails/init"
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,5 +1,5 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# Copyright (C) 2008-2011 AMEE UK Ltd. - http://www.amee.com
+# Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
 
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
