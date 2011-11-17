155b81cf5aa3c5189f50cdbb86364090b956b0cb
James Smith <james@floppy.org.uk>
2011-11-17 15:18:15 +0000

    Add require for core string extensions. SDK-201

--- lib/amee/v3/connection.rb
+++ lib/amee/v3/connection.rb
@@ -1,6 +1,8 @@
 # Copyright (C) 2008-2011 AMEE UK Ltd. - http://www.amee.com
 # Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
 
+require 'active_support/core_ext/string'
+
 module AMEE
   class Connection
 
