fb91bf8c09cb1aad5ba8dbf6be1ed836d9b77be7
James Smith <james@floppy.org.uk>
2012-01-19 11:44:40 +0000

    Include required files for batch operation XML body creation

--- lib/amee/profile_item.rb
+++ lib/amee/profile_item.rb
@@ -1,6 +1,9 @@
 # Copyright (C) 2008-2011 AMEE UK Ltd. - http://www.amee.com
 # Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
 
+require 'active_support/core_ext/hash/conversions'
+require 'active_support/inflector'
+
 module AMEE
   module Profile
     class Item < AMEE::Profile::Object
