a9442fb9ad604acebaf5b9ec6290cd052d86f2c8
James Smith <james@floppy.org.uk>
2012-03-12 17:26:24 +0000

    Allow v3 to be disabled. See PL-11196
    For test purposes.

--- lib/amee/v3.rb
+++ lib/amee/v3.rb
@@ -4,7 +4,11 @@
 require 'amee/v3/meta_helper'
 require 'amee/v3/collection'
 require 'amee/v3/connection'
-require 'amee/v3/item_definition'
-require 'amee/v3/item_value_definition'
-require 'amee/v3/item_value_definition_list'
-require 'amee/v3/return_value_definition'
+
+# Allow v3 code to be completely disabled, for test purposes. See PL-11196
+unless defined?(DISABLE_AMEECONNECT_V3)
+  require 'amee/v3/item_definition'
+  require 'amee/v3/item_value_definition'
+  require 'amee/v3/item_value_definition_list'
+  require 'amee/v3/return_value_definition'
+end
